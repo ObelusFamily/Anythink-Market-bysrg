6a7e42faa85887f638a4a46cdbc995159364fdc5
poisondminds <ryanburns31892@gmail.com>
2022-08-26 22:53:22 -0700

    seed users script

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,1 +1,53 @@
-//TODO: seeds script should come here, so we'll be able to put some data in our local env
+console.log("hello");
+require("../models/User");
+require("../models/Item");
+require("../models/Comment");
+var mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI);
+var User = mongoose.model("User");
+var Item = mongoose.model("Item");
+var Comment = mongoose.model("Comment");
+
+User.deleteMany().then(() => {
+    Item.deleteMany().then(() => {
+        Comment.deleteMany().then(() => {
+            seed();
+        })
+    });
+});
+
+function seed() {
+    const total = 20;
+
+    for (var i = 0; i < total; i++) {
+        const userSlug = "test" + i;
+    
+        const user = new User();
+        user.username = userSlug;
+        user.email = userSlug + "@anythink.com";
+        user.setPassword(userSlug);
+    
+        user.save()
+            .then(() => {
+                const item = new Item();
+                item.slug = user.username;
+                item.title = "Item title " + userSlug;
+                item.seller = user;
+
+                item.save()
+                    .then(() => {
+                        const comment = new Comment();
+                        comment.body = "This is a comment";
+                        comment.item = item;
+                        comment.seller = item.seller;
+
+                        comment.save()
+                            .then(() => {
+                                console.log(userSlug + " creation completed");
+                            })
+                    });
+            })
+        
+    }
+}
